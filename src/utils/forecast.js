09b4ef641fe03e83704306f39da6b87c79b232e9
Maulik Patel <maulikdholariya14@gmail.com>
2021-03-04 22:53:56 +0530

    add humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,6 @@
 const request = require('postman-request')
 
+
 const forecast = (latitude, longitude, callback) => {    
 
     const url ='http://api.weatherstack.com/current?access_key=60c9d5bdf903f47a5d78db6bdfbaa810&query='
@@ -13,8 +14,8 @@ const forecast = (latitude, longitude, callback) => {
 
             callback('Unable to find Location', undefined)
         }else{
-
-            callback(undefined,body.location.name +' '+  body.current.weather_descriptions[0] + ' It is currently ' + body.current.temperature + ' Degress out. It is feels like ' + body.current.feelslike + ' degress out.')
+            
+            callback(undefined,body.location.name +' '+  body.current.weather_descriptions[0] + ' It is currently ' + body.current.temperature + ' Degress out. It is feels like ' + body.current.feelslike + ' degress out. The humidity is '+ body.current.humidity + "%.")
         }
 
 
